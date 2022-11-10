e58c2ec085cdb2945bc666d84b3a4c4b0eebd57d
samsong1019 <samuelsong18@gmail.com>
2022-11-10 18:36:00 -0500

    working routes and added readme

---
+++ README.md
@@ -0,0 +1,36 @@
+# NoteTaker
+
+## Github URL 
+
+https://github.com/samsong1019
+
+## Description 
+```
+This project was made using starter code. the primary focus was to  makes routes. and using  express.  
+GIVEN a note taker app
+WHEN I click the get started button
+THEN I am taken to a new page 
+WHEN i type in a title and text and click save
+THEN my notes will be saved
+```
+## Table of Contents 
+* [Installations](#Installations(Requirments))
+* [Usage](#usage)
+
+* [Contributors](#ContributionGuidelines)
+## Installations(Requirments) 
+To install dependencies, run these commands:
+```
+express and node through npm
+```
+
+## Usage 
+```
+this application is used to take and save notes
+```
+
+## ContributionGuidelines
+```
+you can make a new branch and push and add plenty of commments and also contact me at my email.
+You can also contact me at samuelsong18@gmail.com!
+```
--- server.js
+++ server.js
@@ -1,20 +1,39 @@
-const { urlencoded } = require('express');
-const express = require('express');
-let notes = require('./db/db.json')
-const fs = require('fs')
+// dependencies
+const { urlencoded } = require("express");
+const express = require("express");
+let notes = require("./db/db.json");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
-const path = require('path');
-const { notStrictEqual } = require('assert');
+const path = require("path");
+const { notStrictEqual } = require("assert");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(urlencoded({extended: true}));
+app.use(urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/api/notes", (req, res) => {
-    return res.json(notes)
+  return res.json(notes);
 });
+
+app.post("/api/notes", (req, res) => {
+  let newNote = { title: req.body.title, text: req.body.text };
+  let dbNotes = notes;
+  dbNotes.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(dbNotes));
+  res.json(newNote);
+});
+// order here does matter and we want this to come first becuase * will catch any URL.if not the default will be the app.get 
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => console.log(`Express listening on port ${PORT}`));
