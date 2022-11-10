3c86247e0da74964bcf086be1dc46a5721274eb1
samsong1019 <samuelsong18@gmail.com>
2022-11-09 20:25:43 -0500

    having may routing errors deleted routes foloder to try a different approach

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.send.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 // handles data parsing
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname + "/public")));
 
 // order here matters so always have the api-route before the html-routes since the data must display on to the html pages
-require("./app/routing/api-routes.js")(app);
+require("/routes/api-routes.js")(app);
 
-require("./app/routes/html-routes.js")(app);
+require("/routes/html-routes.js")(app);
 
 app.listen(PORT, function() {
     console.log('App listening on PORT: ' + PORT)
