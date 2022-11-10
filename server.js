3918d8814d6c717654b3e1c19c9d1de7cd69e9e5
samsong1019 <samuelsong18@gmail.com>
2022-11-09 20:28:47 -0500

    tired going to take a break, added dependencies and port/path

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-// Dependencies
-const express = require("express");
-const path = require("path");
+const { urlencoded } = require('express');
+const express = require('express');
+let notes = require('./db/db.json')
+const fs = require('fs')
+
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+const { notStrictEqual } = require('assert');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-// handles data parsing
-app.use(express.urlencoded({ extended: true }));
 
-// parse application/json
-app.use(express.json());
-// starts in public when looking for something
-app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.json())
 
-// order here matters so always have the api-route before the html-routes since the data must display on to the html pages
-require("/routes/api-routes.js")(app);
+app.use(urlencoded({extended: true}));
 
-require("/routes/html-routes.js")(app);
+app.use(express.static('public'));
 
-app.listen(PORT, function() {
-    console.log('App listening on PORT: ' + PORT)
-})
+app.get("/api/notes", (req, res) => {
+    return res.json(notes)
+});
