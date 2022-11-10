b6b7787234681cc1a4066019ee9e4f4312d204ef
samsong1019 <samuelsong18@gmail.com>
2022-11-09 20:00:58 -0500

    cleaned file structure to make it easier to navigate and also added html routes need to finish api routes, finihsed server.js so far i think

---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.send.PORT || 3001;
+// handles data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(express.json());
+// starts in public when looking for something
+app.use(express.static(path.join(__dirname + "/public")));
+
+// order here matters so always have the api-route before the html-routes since the data must display on to the html pages
+require("./app/routing/api-routes.js")(app);
+
+require("./app/routes/html-routes.js")(app);
+
+app.listen(PORT, function() {
+    console.log('App listening on PORT: ' + PORT)
+})
